bb7179fd7f8774177de99acfe9bc5127c038eb31
sapan <sapan8bit@gmail.com>
2019-06-17 11:53:31 +0530

    Adding Templates

--- SQL/2. Insert/Insert_SMSTemplates.sql
+++ SQL/2. Insert/Insert_SMSTemplates.sql
@@ -1,5 +1,27 @@
+-- TRUNCATE TABLE `SMSTemplates`;
+
 INSERT INTO `SMSTemplates` (`templateName`,`message`,`languageID`)
-SELECT * FROM (SELECT 'OTP','Dear user, you verification code is <OTP>.','English' ) AS tmp
+SELECT * FROM (SELECT 'OTP','Dear User,%0D%0A%0D%0AYou verification code is <OTP>.','English' ) AS tmp
 WHERE NOT EXISTS (
     SELECT * FROM `SMSTemplates` WHERE `templateName` = 'OTP' AND `languageID` = 'English'
 ) LIMIT 1;
+
+INSERT INTO `SMSTemplates` (`templateName`,`message`,`languageID`)
+SELECT * FROM (SELECT 'welcomeMessage','<Customer Name>,%0D%0A%0D%0AThanks For Joining <Company Name>.','English' ) AS tmp
+WHERE NOT EXISTS (
+    SELECT * FROM `SMSTemplates` WHERE `templateName` = 'welcomeMessage' AND `languageID` = 'English'
+) LIMIT 1;
+
+INSERT INTO `SMSTemplates` (`templateName`,`message`,`languageID`)
+SELECT * FROM (SELECT 'resetPassword','<Customer Name>,%0D%0A%0D%0Ayour Reset Password Code is This <OTP>.','English' ) AS tmp
+WHERE NOT EXISTS (
+    SELECT * FROM `SMSTemplates` WHERE `templateName` = 'resetPassword' AND `languageID` = 'English'
+) LIMIT 1;
+
+
+INSERT INTO `SMSTemplates` (`templateName`,`message`,`languageID`)
+SELECT * FROM (SELECT 'AccountLock','Dear <Customer Name>,%0D%0A%0D%0AYour  Account lock for <LockTime> Min .Due to <INVALID ATTEMPT> INVALID ATTEMPT.','English' ) AS tmp
+WHERE NOT EXISTS (
+    SELECT * FROM `SMSTemplates` WHERE `templateName` = 'AccountLock' AND `languageID` = 'English'
+) LIMIT 1;
+
