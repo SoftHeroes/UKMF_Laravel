bb86817fb3bb8e2a40ad43dcd7130849bea40f82
subham <subham@ukmf.in>
2019-07-15 12:45:57 +0530

    Dividing Code of Schema

--- SQL/1. Schema/1. Schema_01.sql
+++ SQL/1. Schema/1. Schema_01.sql
@@ -271,12 +271,3 @@ CREATE TABLE `loginSetup` (
 	`Deleted` INT(1) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`u_ID`)
 );
-
-DROP TABLE IF EXISTS `lookUp`;
-CREATE TABLE `lookUp` (
-  `code` INT(10) NOT NULL,
-  `name` VARCHAR(1000) NOT NULL,
-  `languageID` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-	PRIMARY KEY (`code`,`name`)
-);
---
+++ SQL/1. Schema/2. Schema_02.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS `lookUp`;
+CREATE TABLE `lookUp` (
+  `code` INT(10) NOT NULL,
+  `name` VARCHAR(1000) NOT NULL,
+  `languageID` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+	PRIMARY KEY (`code`,`name`)
+);
