1684037ce2258157b3d879f62dd7e1fcc8a6c686
subham <subham@ukmf.in>
2019-07-15 12:50:17 +0530

    Correcting Issues of Not Null indexing issue

--- SQL/1. Schema/1. Schema.sql
+++ SQL/1. Schema/1. Schema.sql
@@ -271,3 +271,12 @@ CREATE TABLE `loginSetup` (
 	`Deleted` INT(1) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`u_ID`)
 );
+
+DROP TABLE IF EXISTS `lookUp`;
+CREATE TABLE `lookUp` (
+  `code` INT(10) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `languageID` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+	PRIMARY KEY (`code`,`name`)
+);
--- SQL/1. Schema/2. Schema_02.sql
+++
@@ -1,8 +0,0 @@
-﻿DROP TABLE IF EXISTS `lookUp`;
-CREATE TABLE `lookUp` (
-  `code` INT(10) NOT NULL,
-  `name` VARCHAR(1000) NOT NULL,
-  `languageID` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-	PRIMARY KEY (`code`,`name`)
-);
