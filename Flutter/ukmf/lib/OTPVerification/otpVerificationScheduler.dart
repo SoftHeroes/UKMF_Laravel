7d4ec0fa010e76b1f5d538384b4657169a0ea249
subham <subham@ukmf.in>
2019-07-23 18:05:54 +0530

    correcting code of provider

--- Flutter/ukmf/lib/OTPVerification/otpVerificationScheduler.dart
+++ Flutter/ukmf/lib/OTPVerification/otpVerificationScheduler.dart
@@ -3,60 +3,56 @@ import 'package:http/http.dart';
 
 class OTPVerificationScheduler with ChangeNotifier {
   bool _isResendingOTP = false;
-  bool _isCanResendOTP = true;
-  bool _isShowingLoader = true;
-
-  String _mobileNumber = '';
-  String _countryCode = '';
-  String _otp = '';
-  String _enterOTP = '';
-  Response _response;
-
   bool get isResendingOTP => _isResendingOTP;
-  bool get isCanResendOTP => _isCanResendOTP;
-  bool get isShowingLoader => _isShowingLoader;
-
-  String get mobileNumber => _mobileNumber;
-  String get countryCode => _countryCode;
-  String get otp => _otp;
-  String get enterOTP => _enterOTP;
-  Response get response => _response;
-
   set isResendingOTP(bool newValue) {
     _isResendingOTP = newValue;
     notifyListeners();
   }
 
+  bool _isCanResendOTP = true;
+  bool get isCanResendOTP => _isCanResendOTP;
   set isCanResendOTP(bool newValue) {
     _isCanResendOTP = newValue;
     notifyListeners();
   }
 
+  bool _isShowingLoader = true;
+  bool get isShowingLoader => _isShowingLoader;
   set isShowingLoader(bool newValue) {
     _isShowingLoader = newValue;
     notifyListeners();
   }
 
+  String _mobileNumber = '';
+  String get mobileNumber => _mobileNumber;
   set mobileNumber(String newValue) {
     _mobileNumber = newValue;
     notifyListeners();
   }
 
+  String _countryCode = '';
+  String get countryCode => _countryCode;
   set countryCode(String newValue) {
     _countryCode = newValue;
     notifyListeners();
   }
 
+  String _otp = '';
+  String get otp => _otp;
   set otp(String newValue) {
     _otp = newValue;
     notifyListeners();
   }
 
+  String _enterOTP = '';
+  String get enterOTP => _enterOTP;
   set enterOTP(String newValue) {
     _enterOTP = newValue;
     notifyListeners();
   }
 
+  Response _response;
+  Response get response => _response;
   set response(Response newValue) {
     _response = newValue;
     notifyListeners();
--- Flutter/ukmf/lib/mobileNumberVerification/mobileNumberVerificationScheduler.dart
+++ Flutter/ukmf/lib/mobileNumberVerification/mobileNumberVerificationScheduler.dart
@@ -3,37 +3,35 @@ import 'package:http/http.dart';
 
 class MobileNumberVerificationScheduler with ChangeNotifier {
   bool _isGettingOTP = false;
-  bool _isIAgree = false;
-  String _mobileNumber = "";
-  String _countryCode = "+91";
-  Response _response;
-
   bool get isGettingOTP => _isGettingOTP;
-  bool get isIAgree => _isIAgree;
-  String get mobileNumber => _mobileNumber;
-  String get countryCode => _countryCode;
-  Response get response => _response;
-
   set isGettingOTP(bool newValue) {
     _isGettingOTP = newValue;
     notifyListeners();
   }
 
-  set mobileNumber(String newValue) {
-    _mobileNumber = newValue;
-    notifyListeners();
-  }
-
-  set countryCode(String newValue) {
-    _countryCode = newValue;
-    notifyListeners();
-  }
-
+  bool _isIAgree = false;
+  bool get isIAgree => _isIAgree;
   set isIAgree(bool newValue) {
     _isIAgree = newValue;
     notifyListeners();
   }
 
+  String _mobileNumber = "";
+  String get mobileNumber => _mobileNumber;
+  set mobileNumber(String newValue) {
+    _mobileNumber = newValue;
+    notifyListeners();
+  }
+
+  String _countryCode = "+91";
+  String get countryCode => _countryCode;
+  set countryCode(String newValue) {
+    _countryCode = newValue;
+    notifyListeners();
+  }
+
+  Response _response;
+  Response get response => _response;
   set response(Response newValue) {
     _response = newValue;
     notifyListeners();
