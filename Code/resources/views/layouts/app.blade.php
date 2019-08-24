8cf3f4569e8d543ef758c759c1cd54295070e70c
SOftHeroes <ShubhamJobanputra@gmail.com>
2019-08-24 05:20:20 -0700

    Correcting JS path

--- Code/resources/views/layouts/app.blade.php
+++ Code/resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
     <script src="{{ asset('js/inputValidation.js') }}"></script>
     <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
     <script src=" {{asset('js/popper.min.js')}}"></script>
-    <script src="{{asset('bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <script src="{{asset('js/main.js')}}"></script>
     <!-- The javascript plugin to display page loading on top-->
     <script src=" {{asset('js/plugins/pace.min.js')}}"></script>
